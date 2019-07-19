4b99d1ec82bed75506e4e69c07e1d5f8dc644521
justin.laxamana <justrlax@gmail.com>
2019-07-19 00:39:26 -0500

    BIG update

--- _assets/js/scripts/1_menu.js
+++ _assets/js/scripts/1_menu.js
@@ -1,10 +1,20 @@
-
-$('.with-dropdown').on("click", function(e){
-  $(this).toggleClass('active');
-  $(this).next('.dropdown').toggleClass('active');
+// Menu Click
+$('.main-header__menu').on("click", function(e){
+  $('.main-header').toggleClass('active');
+  if($('.main-header').hasClass('active')) {
+    $('.main-header').append('<span class="close"></span>');
+  } else {
+    $('.close').remove();
+  }
   e.preventDefault();
 });
 
-$('.dropdown a').on("click", function(){
-  $('.dropdown').removeClass('active');
+$('.main-header').on("click",'.close', function(){
+  $('.main-header').removeClass('active');
+  $(this).remove();
+});
+
+$('.main-header nav').on("click",'a', function(){
+  $('.main-header').removeClass('active');
+  $('.main-header .close').remove();
 });
